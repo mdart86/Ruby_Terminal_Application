33d88dac2c09e211e66f9c0abbf927249575c384
Michael Dart <mdart86@hotmail.com>
2020-12-16 17:09:01 +1000

    creation of script

--- src/classes.rb
+++ src/classes.rb
@@ -8,18 +8,16 @@ class Member
         @home_city = member_details[:home_city]
         @age = member_details[:age]
         @@member_id += 1
-        @member_number = 
     end
-
+    
     def to_s
         return "Welcome to the Quiz app #{@first_name} from #{@home_city}. Your unique member number is #{@@member_id}. Keep this handy, you'll need it when you come back!"
     end
 end
 # MEMBER CLASS END
 
-
 # class Existing_Member
-#     attr_reader:
+    # attr_reader:
 # end
 
 # CAPITAL CITY EASY CLASS START
@@ -47,7 +45,8 @@ class Capital_City_Hard
     attr_accessor :prompt, :answer
     def initialize(prompt, answer)
         @prompt = prompt
-        @answer = answer
+        @answer = answer 
     end
 end
+
 # CAPITAL CITY HARD CLASS END
--- src/index.rb
+++ src/index.rb
@@ -1,6 +1,5 @@
 require 'yaml'
-require_relative ("./questions_answers.rb")
-require_relative ("./classes.rb")
+require_relative ("classes.rb")
 
 class InvalidMemberDetails < StandardError
 end
@@ -75,11 +74,109 @@ end
 def capital_city_menu
     input_options_array_capital_city = ["1","2","3","4","5"]
     puts "You are now in the Capital City menu! Please select your quiz"
-    puts "Enter (1) to try the EASY capital city Quiz"
-    puts "Enter (1) to try the MEDIUM capital city Quiz"
-    puts "Enter (1) to try the HARD capital city Quiz"
+    puts "Enter (1) to try the EASY quiz"
+    puts "Enter (2) to try the MEDIUM quiz"
+    puts "Enter (3) to try the HARD quiz"
     puts "Press (4) to go back to the Quiz Menu"
 end
+
+def score_menu
+    input_options_array_quiz = ["1","2","3","4"]
+    puts "You are now in the quiz menu! Please select your quiz"
+    puts "Enter (1) to show your Capital Cities Quiz scores"
+    puts "Enter (2) to show your Countries Quiz scores"
+    puts "Enter (3) to show your Geography Quiz scores"
+    puts "Press (4) to Back to Main Menu"
+end
+
+# CAPITAL CITY EASY QUESTIONS ANSWER AND METHOD START
+    p1 = "What is the capital city of Australia?\n(A) Sydney\n(B) Canberra\n(C) Melbourne"
+    p2 = "What is the capital city of France?\n(A) Nice\n(B) Lyon\n(C) Paris"
+    p3 = "What is the capital city of Japan?\n(A) Tokyo\n(B) Osaka\n(C) Nagoya"
+    p4 = "What is the capital city of the USA?\n(A) New York City\n(B) Washington D.C.\n(C) Los Angeles"
+    p5 = "What is the capital city of New Zealand?\n(A) Auckland\n(B) Christchurch\n(C) Wellington"
+    
+    questions_capital_city_easy = [
+        Capital_City_Easy.new(p1, "B"),
+        Capital_City_Easy.new(p2, "C"),
+        Capital_City_Easy.new(p3, "A"),
+        Capital_City_Easy.new(p4, "B"),
+        Capital_City_Easy.new(p5, "C")
+    ]
+
+    def run_test(questions_capital_city_easy)
+        answer = ""
+        score = 0
+        for question in questions_capital_city_easy
+            puts question.prompt
+            answer = gets.chomp()
+            if answer.upcase! == question.answer
+                score += 1
+            end
+            puts "You got" " " + score.to_s + "/" + questions_capital_city_easy.length().to_s
+            $score_cc_easy = []
+            $score_cc_easy < score
+        end       
+    end
+# CAPITAL CITY EASY QUESTIONS ANSWER AND METHOD START
+
+# CAPITAL CITY MEDIUM QUESTIONS ANSWER AND METHOD START
+p1 = "What is the capital city of Italy?\n(A) Milan\n(B) Rome\n(C) Venice"
+p2 = "What is the capital city of China?\n(A) Shanghai\n(B) Beijing\n(C) Hong Kong"
+p3 = "What is the capital city of Canada?\n(A) Vancouver\n(B) Toronto\n(C) Ottawa"
+p4 = "What is the capital city of Malta?\n(A) Sliema\n(B) Valletta\n(C) Mdina"
+p5 = "What is the capital city of Thailand?\n(A) Phuket\n(B) Chiang Mai\n(C) Bangkok"
+
+questions_capital_city_medium = [
+    Capital_City_Medium.new(p1, "B"),
+    Capital_City_Medium.new(p2, "B"),
+    Capital_City_Medium.new(p3, "C"),
+    Capital_City_Medium.new(p4, "B"),
+    Capital_City_Medium.new(p5, "C")
+]
+
+    def run_test(questions_capital_city_medium)
+        answer = ""
+        score = 0
+        for question in questions_capital_city_medium
+            puts question.prompt
+            answer = gets.chomp()
+            if answer.upcase! == question.answer
+                score += 1
+            end
+        end
+        puts "You got" " " + score.to_s + "/" + questions_capital_city_medium.length().to_s
+    end
+# CAPITAL CITY MEDIUM QUESTIONS ANSWER AND METHOD END
+
+# CAPITAL CITY HARD QUESTIONS ANSWER AND METHOD START
+p1 = "What is the capital city of Turkey?\n(A) Istanbul\n(B) Ankara\n(C) Alanya"
+p2 = "What is the capital city of Brazil?\n(A) Sao Paulo\n(B) Brasilia\n(C) Rio De Janeiro"
+p3 = "What is the capital city of Switzerland?\n(A) Bern\n(B) Geneva\n(C) Zurich"
+p4 = "What is the capital city of Morocco?\n(A) Rabat\n(B) Casablanca\n(C) Marrakesh"
+p5 = "What is the capital city of Colombia?\n(A) Cali\n(B) Medellin\n(C) Bogota"
+
+questions_capital_city_hard = [
+    Capital_City_Hard.new(p1, "B"),
+    Capital_City_Hard.new(p2, "B"),
+    Capital_City_Hard.new(p3, "A"),
+    Capital_City_Hard.new(p4, "A"),
+    Capital_City_Hard.new(p5, "C")
+]
+
+    def run_test(questions_capital_city_hard)
+        answer = ""
+        score = 0
+        for question in questions_capital_city_hard
+            puts question.prompt
+            answer = gets.chomp()
+            if answer.upcase! == question.answer
+                score += 1
+            end
+        end
+        puts "You got" " " + score.to_s + "/" + questions_capital_city_hard.length().to_s
+    end
+# CAPITAL CITY HARD QUESTIONS ANSWER AND METHOD END
 
 while true
     input_options_array = ["1","2","3","4","5"]
@@ -127,9 +224,9 @@ while true
                         while true
                             input_options_array_capital_city = ["1","2","3","4"]
                             puts "Welcome to the Capital City Quiz Menu! Please select the quiz level you want to do"
-                            puts "Enter (1) to try the EASY capital city Quiz"
-                            puts "Enter (1) to try the MEDIUM capital city Quiz"
-                            puts "Enter (1) to try the HARD capital city Quiz"
+                            puts "Enter (1) to try the EASY quiz"
+                            puts "Enter (2) to try the MEDIUM quiz"
+                            puts "Enter (3) to try the HARD quiz"
                             puts "Press (4) to go back to the Quiz Menu"
                             input_option = gets.strip
                             if !input_options_array_capital_city.include?(input_option)
@@ -140,14 +237,29 @@ while true
                         when "1"
                             puts "Loading the EASY capital city quiz!"
                             run_test(questions_capital_city_easy)
+                            sleep(0.5)
+                            puts "Saving your score, check out the scores menu to see"
+                            sleep(1)
+                            puts "Returning back to the Capital City Quiz Menu..."
+                            sleep(1)
                             # figure out why I can't use methods across pages
                         when "2"
                             puts "Loading the MEDIUM capital city quiz!"
                             run_test(questions_capital_city_medium)
+                            sleep(0.5)
+                            puts "Saving your score, check out the scores menu to see"
+                            sleep(1)
+                            puts "Returning back to the Capital City Quiz Menu..."
+                            sleep(1)
                             # figure out why I can't use methods across pages
                         when "3"
                             puts "Loading the HARD capital city quiz!"
                             run_test(questions_capital_city_hard)
+                            sleep(0.5)
+                            puts "Saving your score, check out the scores menu to see"
+                            sleep(1)
+                            puts "Returning back to the Capital City Quiz Menu..."
+                            sleep(1)
                             # figure out why I can't use methods across pages
                         when "4"
                             puts "Returning to the quiz menu"
@@ -169,7 +281,40 @@ while true
                 end # end of case statement
             end # End of while loop
         when "4"
-            puts "Showing your answers and score"
+            puts "Loading the score menu"
+            sleep(1)
+                while true
+                input_options_array_quiz = ["1","2","3","4"]
+                puts "You are now in the quiz menu! Please select your quiz"
+                puts "Enter (1) to show your Capital Cities Quiz scores"
+                puts "Enter (2) to show your Countries Quiz scores"
+                puts "Enter (3) to show your Geography Quiz scores"
+                puts "Press (4) to Back to Main Menu"
+                input_option = gets.strip 
+                if !input_options_array.include?(input_option)
+                    puts "Expecting an input of: 1,2,3,4 OR 5"
+                    next
+                end
+                case input_option
+                    when "1"
+                        puts "Loading Capital City Quiz Scores"
+                        sleep(1)
+                        puts "Your score for the EASY quiz is: " + $score_cc_easy.to_s + "/ 5" 
+                        # puts "Your score for the MEDIUM quiz is: " $score_cc_medium "/ 5"   
+                        # puts "Your score for the HARD quiz is: " $score_cc_hard "/ 5" 
+                        if input_option == "4"
+                            main_menu
+                        end
+                    when "2"
+                        puts "Loading Countries Quiz Scores"   
+                    when "3"
+                        puts "Loading Geography Quiz Scores"   
+                    when "4"
+                    puts "Going back to main menu"
+                    main_menu
+                break
+                end # end of case statement
+            end # End of while loop
         when "5"
             puts "Exiting from the Quiz App ..."
             break
