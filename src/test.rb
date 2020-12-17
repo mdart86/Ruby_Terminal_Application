daf3d85c64efd446a25614e3ae5761d537385dae
Michael Dart <mdart86@hotmail.com>
2020-12-18 09:16:34 +1000

    additional testing to get capital city menu working

--- src/test.rb
+++ src/test.rb
@@ -39,15 +39,18 @@ require_relative ("classes.rb")
 $prompt = TTY::Prompt.new
 
 def select_option
-    return $prompt.select("Choose from one of the following options", ["Create a new profile", "Load an existing profile", "Go to Quiz Menu", "Show your Scores", "Exit"])
+    return $prompt.select("Choose from one of the following options", 
+    ["Create a new profile", "Load an existing profile", "Go to Quiz Menu", "Show your Scores", "Exit"])
 end
 
 def quiz_menu
-    return $prompt.select("Choose from one of the quiz categories", ["Capital Cities", "Countries", "Geography", "Back to Main Menu"])
+    return $prompt.select("Choose from one of the quiz categories", 
+    ["Capital Cities", "Countries", "Geography", "Back to Main Menu"])
 end
 
 def city_menu
-    return $prompt.select("Choose from one of quiz levels", ["Easy", "Medium", "Hard", "Back to the Quiz Menu"])
+    return $prompt.select("Choose from one of quiz levels", 
+    ["Easy", "Medium", "Hard", "Back to the Quiz Menu"])
 end
 
 answer = ""
@@ -71,9 +74,10 @@ while answer != "Exit"
         sleep(1)
         quiz_menu
             when "Capital Cities"
-            puts " "
-            city_menu            
+            puts " "           
             sleep(1)
+            city_menu
+            puts " "     
             when "Easy"
                 run_test(questions_capital_city_easy)
                 sleep(0.5)
